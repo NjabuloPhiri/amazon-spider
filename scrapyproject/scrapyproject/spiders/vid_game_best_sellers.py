0b8a43ff51f26c2c3081acd66491bc9b7b0e2f4c
NjabuloPhiri <njabulophiri203@gmail.com>
2024-06-14 20:49:13 +0200

    first success at extracting price values

--- scrapyproject/scrapyproject/spiders/vid_game_best_sellers.py
+++ scrapyproject/scrapyproject/spiders/vid_game_best_sellers.py
@@ -4,7 +4,7 @@ from scrapy import Selector
 
 class VidGameBestSellersSpider(scrapy.Spider):
     name = "vid-game-best-sellers"
-    allowed_domains = ["tinyurl.com", "amazon.co.za"]
+    allowed_domains = ["amazon.co.za"]
 
     # The original start_url for this project points to the
     # Amazon.co.za Best Seller section for
@@ -23,7 +23,7 @@ class VidGameBestSellersSpider(scrapy.Spider):
         # `start_requests()` is a response method that retrieves raw contents 
         # from the target URL and then passes the response to the `parse()`
         # method.
-        start_urls = ["https://tinyurl.com/4swm2a8b"]
+        start_urls = ["https://www.amazon.co.za/gp/bestsellers/videogames?ref_=Oct_d_obs_S"]
         for url in start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -31,19 +31,32 @@ class VidGameBestSellersSpider(scrapy.Spider):
         # The `parse()` method takes raw contents in the `response`
         # variable and yields/returns the parsed data after applying
         # the relevant parsing.
-        divs = response.xpath("//div")
+        
+        # response.xpath("//div/span[contains(@class, 'p13n-sc-price')]").getall()
+        # ☝🏾 The query above returns all the product prices for
+        # best sellers in the video games category.
 
-        for best_seller in response.xpath("//div").getall(): # This is the <div> class wrapping all best-sellers
+        for best_seller in response.css("div.a-row"): # This is the <div> class wrapping all best-sellers
             # We want to extract:
             # (1) product_name,
             # (2) price
+            one = best_seller.css("span")
+            two = best_seller.css("span")
+            product_name = one.css("span.a-size-small.a-color-base::text").get()
+            price = two.css("span.p13n-sc-price::text").get()
+
             yield {
-                "product_name": best_seller.css("._cDEzb_p13n-sc-css-line-clamp-1_1Fn1y ::text").get(),
-                "price": best_seller.css("//span[@class='_cDEzb_p13n-sc-price_3mJ9Z']/text()").get(),
+                "product_name": product_name,
+                "price": price,
             }
+
+            # yield {
+            #     "product_name": best_seller.css("._cDEzb_p13n-sc-css-line-clamp-1_1Fn1y ::text").get(),
+            #     "price": best_seller.css("//span[@class='_cDEzb_p13n-sc-price_3mJ9Z']/text()").get(),
+            # }
     
     # TO-DO: 
     # 14/06/2024
-    # Debug script to get it to recognize `.get()`, `xpath()`, and `.css()`
+    # Further work the script so that the data saved to the csv file is clean.
 
     
